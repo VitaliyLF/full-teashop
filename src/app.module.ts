091237d24e50824c46776b19ff594c118030ec07
VitaliyLF <97784759+Vetosy@users.noreply.github.com>
2025-09-23 20:07:53 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   // imports: [], // сюда подключаются другие модули (например, UsersModule, AuthModule)
   // controllers: [AppController], // сюда подключаются контроллеры
   // providers: [AppService]s // сюда подключаются сервисы
-  imports: [ConfigModule.forRoot()] // чтобы читать env значения
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule] // чтобы читать env значения
 })
 export class AppModule {}
