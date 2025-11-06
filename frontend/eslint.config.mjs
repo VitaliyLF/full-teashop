f7431ac63e01bf8163b9f6a019caaa1d3df97c25
VitaliyLF <97784759+Vetosy@users.noreply.github.com>
2025-11-06 21:57:21 +0300

    Update

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,33 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'error',
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      'no-else-return': 'warn',
+      'no-var': 'warn',
+      'no-await-in-loop': 'error',
+      'no-duplicate-imports': 'error',
+      'prefer-const': 'warn',
+      camelcase: ['warn', { properties: 'never' }],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,44 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
+// Включаем все в nextConfig чтобы env норм работало
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  // настройка env файла
+  env: {
+    // указываем все наши env переменные из файла .env
+    APP_ENV: process.env.APP_ENV,
+    APP_URL: process.env.APP_URL,
+    APP_DOMAIN: process.env.APP_DOMAIN,
+    SERVER_URL: process.env.SERVER_URL,
+  },
+  // настройка картинок
+  // в next если мы используем картинки со стороннего сайта нужно указать images
+  // а мы для юзеров которые авторизовались через яндекс или еще используем картинки профеля этих сервисов
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.yandex.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 
-export default nextConfig;
+  // функция rewrites она нужно чтобы подключать файлы с сервера
+  // uploads/products/... вот эти картинки
+  async rewrites() {
+    return [
+      {
+        // указываем путь до папки
+        source: '/uploads/:path*',
+        // указываем путь до сервера
+        destination: `${process.env.SERVER_URL}/uploads/:path*`,
+      },
+    ]
+  },
+}
+
+export default nextConfig
