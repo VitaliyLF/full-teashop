ed8a4ee167f1c4f928f0e08223af8517e3fef619
VitaliyLF <97784759+Vetosy@users.noreply.github.com>
2025-11-07 20:45:40 +0300

    update

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,29 +1,30 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { type PropsWithChildren, useState } from 'react'
+// для дебагинга
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { type PropsWithChildren } from 'react'
 import { Toaster } from 'react-hot-toast'
+
+// tanstack react query библиотека для запросов
+// подключаем так по документациии
+const queryClient = new QueryClient({
+  // указываем настройки
+  defaultOptions: {
+    // эта настройка нужно что при изменении фокуса окна бразуера не происходил лишний запрос
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 // Сюда подключается все библиотеки провайдеры
 const Providers = ({ children }: PropsWithChildren) => {
-  // tanstack react query библиотека для запросов
-
-  // подключаем так по документациии
-  const [client] = useState(
-    new QueryClient({
-      // указываем настройки
-      defaultOptions: {
-        // эта настройка нужно что при изменении фокуса окна бразуера не происходил лишний запрос
-        queries: {
-          refetchOnWindowFocus: false,
-        },
-      },
-    }),
-  )
   return (
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       <Toaster />
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
